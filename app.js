2ddf2be84f6fa2f212cd7fc280f842317263e811
Nidnogg <henriquevt98@gmail.com>
2018-11-04 06:23:19 +0000

    package.json, express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express  = require('express')
+const app = express()
+const port = 3000
+
+console.log("Starting node test server")
+// Home routes
+app.get('/', (req, res) => res.sendFile(__dirname + "./public/index.html"))
+app.get('/home', (req, res) => res.sendFile(__dirname + "./public/index.html"))
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
