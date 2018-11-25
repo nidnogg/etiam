b4d7a36699c781a9cf084756bc73bdfc92f225a8
YURI <yurimedeiros33@gmail.com>
2018-11-25 06:27:25 -0200

    nova estrutura de paginas, ejs templates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express  = require('express')
 
 const app = express()
 const port = 3000
+let ejs = require('ejs')
 
 const conn = mysql.createConnection({
     host: "bdgiseli.cmr2oig5ij0x.sa-east-1.rds.amazonaws.com",
@@ -10,19 +11,23 @@ const conn = mysql.createConnection({
     password: "giselaobd489-db"
 });
 
-conn.connect(function(err) { 
+conn.connect(function(err) {
     if(err) throw err;
     console.log("Conectou ao banco");
 });
 
 console.log("Starting node test server")
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/views/pages/'));
+app.use('/assets', express.static(__dirname + '/public'));
+//ejs view engine
+app.set('view engine', 'ejs')
 
 // Home routes
-app.get('/', (req, res) => res.sendFile(__dirname + "/public/index.html"))
-app.get('/home', (req, res) => res.sendFile(__dirname + "/public/index.html"))
+
+app.get('/', (req, res) => res.render( __dirname + '/public/views/pages/index.ejs'));
+app.get('/home', (req, res) => res.render(__dirname + 'public/views/pages/index.ejs'));
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
-app.listen(port, () => console.log(`Localhost:${port}`))
